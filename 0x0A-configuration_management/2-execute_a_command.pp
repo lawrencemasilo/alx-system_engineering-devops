0924ee1de6c08f9287e0b027420b727be1c0ded9
neo masilo <neolawrencemasilo@gmail.com>
2023-11-24 14:34:20 +0000

    puppet script that kills a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,16 +1,6 @@
 #kills a process named killmenow
-
-exec { 'killmenow_process':
-  command     => '/usr/bin/pkill killmenow',
-  refreshonly => true,
-  logoutput   => true,
-  subscribe   => Exec['check_killmenow_process'],
+exec { 'killmenow':
+  command  => '/usr/bin/pkill killmenow',
+  provider => 'shell',
+  returns  => [0, 1],
 }
-
-exec { 'check_killmenow_process':
-  command     => '/usr/bin/pgrep killmenow',
-  returns     => [0, 1],
-  refreshonly => true,
-  logoutput   => false,
-}
-
